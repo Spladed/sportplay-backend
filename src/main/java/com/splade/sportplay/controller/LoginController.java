20df5e13d6d7ef907231db14e0bf43036823ece2
Spladed <1310140596@qq.com>
2021-03-03 20:01:10 +0800

    从数据库验证登录

--- src/main/java/com/splade/sportplay/controller/LoginController.java
+++ src/main/java/com/splade/sportplay/controller/LoginController.java
@@ -1,17 +1,33 @@
 package com.splade.sportplay.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.splade.sportplay.bean.User;
+import com.splade.sportplay.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class LoginController {
 
+    @Autowired
+    UserDao userDao;
+
     @RequestMapping("/login")
     public String login(@RequestBody User user){
-        System.out.println(user);
-        return "ok";
+        String flag="error";
+        Map<String, Object> map = new HashMap<>();
+        User u=userDao.getUserByMessage(user.getUsername(),user.getPassword());
+        if(u!=null){
+            flag="ok";
+        }
+        map.put("flag",flag);
+        map.put("user",u);
+        return JSON.toJSONString(map);
     }
 
 }
